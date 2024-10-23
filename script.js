b8434e7487ac2e2633954ee657b7e31c7f500554
Kowsar Mahmud Pappu <kowsarmahmud156@gmail.com>
2024-10-23 13:03:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function downloadCV() {
-    window.open('cv.pdf', '_blank');
-  }
+  window.open('cv.pdf', '_blank');
+}
+
+function openTab(tabName) {
+  // Get all tab contents
+  const tabContents = document.querySelectorAll('.tab-content');
   
+  // Hide all tab contents
+  tabContents.forEach(content => {
+    content.style.display = 'none';
+  });
+  
+  // Show the selected tab content
+  document.getElementById(tabName).style.display = 'block';
+}
+
+// Optionally, open the home tab by default
+openTab('home');
